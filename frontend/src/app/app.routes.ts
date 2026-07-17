e7116c71aa1bc87b1cf8b4e8cf8e93960d96f2f9
Ruwanya Hettiarachchi <ruwanyahettiarachchi@gmail.com>
2026-07-17 19:22:14 +0530

    feat(employee): implement angular frontend

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'employees',
+    pathMatch: 'full'
+  },
+  {
+    path: 'employees',
+    loadComponent: () => import('./features/employee/employee-list/employee-list.component')
+      .then(m => m.EmployeeListComponent)
+  },
+  {
+    path: 'employees/new',
+    loadComponent: () => import('./features/employee/employee-form/employee-form.component')
+      .then(m => m.EmployeeFormComponent)
+  },
+  {
+    path: 'employees/edit/:id',
+    loadComponent: () => import('./features/employee/employee-form/employee-form.component')
+      .then(m => m.EmployeeFormComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'employees'
+  }
+];
